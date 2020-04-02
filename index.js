8a0d3f97bf69be10d48503e130fa884319239c18
Sayem Chowdhury <sayem314@gmail.com>
2020-04-02 12:31:03 +0600

    update hooks

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 import React, { useEffect } from "react";
 import { NativeModules } from "react-native";
 
-export function useKeepAwake() {
-  useEffect(() => {
-    NativeModules.KCKeepAwake.activate();
-    return () => NativeModules.KCKeepAwake.deactivate();
-  }, []);
-}
-
 export function activateKeepAwake() {
   NativeModules.KCKeepAwake.activate();
 }
@@ -16,10 +9,17 @@ export function deactivateKeepAwake() {
   NativeModules.KCKeepAwake.deactivate();
 }
 
+export function useKeepAwake() {
+  useEffect(() => {
+    activateKeepAwake();
+    return () => deactivateKeepAwake();
+  }, []);
+}
+
 export default function KeepAwake() {
   useEffect(() => {
-    NativeModules.KCKeepAwake.activate();
-    return () => NativeModules.KCKeepAwake.deactivate();
+    activateKeepAwake();
+    return () => deactivateKeepAwake();
   }, []);
 
   return null;
